0023704d2a18a35f1e2a13f2cd97ae91f3497725
Gbenga Adebiyi <adebiyigs@gmail.com>
2021-04-10 13:37:12 +0100

    Add files via upload

--- split_functions.py
+++ split_functions.py
@@ -6,9 +6,10 @@ from collections import defaultdict
 
 
 def col_plot(df, col, title):
-    '''THis function returns the plot of a column
+    '''
+    This function returns horizontal bar plot of a column in a dataframe df, with a title
     INPUT:
-    survey DataFrame & Column to plot
+    survey DataFrame, Column to plot & the title of the chart
     
     OUTPUT:
     A chart of the values count for the specified column
@@ -23,41 +24,42 @@ def col_plot(df, col, title):
 
 
 def list_options(df, col = 'NEWSOSites'):
-    '''Function to return the number of different options in df['col'] rows as well as return the list of the options
+    '''
+    Function to return the number of different options/string values in a dataframe row as well as return the list of the string         values
+       
     INPUT:
+    DataFrame & Column in the dataframe
     
-    DataFrame & Column
     OUTPUT:
-    Displays the number of options and returns a list of the options
+    Displays the number of string values and returns a list of the string values
     '''
     #declare an empty list
-    options = []
-    
-    #looop through the column
+    string_val = []
+    #loop through the column in the df
     for val in df[col]:
         try:
-            options.extend(val.split(';'))
+            string_val.extend(val.split(';'))
         except AttributeError:
             pass
     #Apply set to remove duplicates in the options. List converts the set object to a list
-    options = list(set(options))
+    string_val = list(set(string_val))
     
-    print("The number of options in {} is {}.".format(col,(len(options))))
+    print("The number of options in {} is {}.".format(col,(len(string_val))))
     
-    return options
+    return string_val
 
 
 def volume_count(df, col1, col2, look_for):
     '''
     This function generates the count of each string (options) in the specified column of a DataFrame
     INPUT:
-    df - the pandas dataframe you want to search
-    col1 - the column name you want to look through
+    df - the pandas dataframe
+    col1 - the column you want to look through
     col2 - the column you want to count values from
     look_for - a list of strings you want to search for in each row of the dataframe column
 
     OUTPUT:
-    new_df - a dataframe of each look_for with the count of how often it shows up
+    new_df - a dataframe of each look_for with the count of how often it appeared in the dataframe column
     '''
     new_df = defaultdict(int)
     
@@ -76,16 +78,15 @@ def volume_count(df, col1, col2, look_for):
     
 def clean_and_plot(df, col = 'NEWSOSites', title='Most Visited Stackoverflow Site', plot=True):
     '''
-    
+    This function plots the porportion of a string value in a dataframe column
     INPUT 
-        df - a dataframe holding the Stackoverflow sites visited column
-        title - string the title of your plot
-        axis - axis object
+        df - a dataframe holding the column
+        title - string the title of the plot
         plot - bool providing whether or not you want a plot back
         
     OUTPUT
         study_df - a dataframe with the count of how many individuals
-        Displays a plot of showing the count of the site visited by developers who answered the questionnaire.
+        Displays a plot of showing the proportion count of the values in the column.
     '''
     site = df[col].value_counts().reset_index()
     site.rename(columns={'index': 'method', col: 'count'}, inplace=True)
